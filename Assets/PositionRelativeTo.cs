22cf08bd9d6172e59989aff0140295bbf5500994
stengard <mstengard@gmail.com>
2016-11-01 16:32:31 +0100

    added a simple adjustment window for resizing objects

---
+++ Assets/PositionRelativeTo.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PositionRelativeTo : MonoBehaviour
+{
+
+    public GameObject PositionRelativeToGameObject;
+    public bool ToTheRight;
+    public bool ToTheLeft;
+    public bool Above;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float offset = 0;
+        Vector3 newPosition = new Vector3();
+	    if (ToTheRight)
+	    {
+
+	        offset = PositionRelativeToGameObject.GetComponent<Collider>().bounds.size.x/2 + 0.1f;
+            newPosition = PositionRelativeToGameObject.transform.position + (PositionRelativeToGameObject.transform.right * offset);
+
+        }
+        else if (ToTheLeft)
+        {
+            offset= -(PositionRelativeToGameObject.GetComponent<Collider>().bounds.size.x/2 + 0.1f);
+            newPosition = PositionRelativeToGameObject.transform.position + (PositionRelativeToGameObject.transform.right * offset);
+        }
+        else if(Above)
+	    {
+            offset = (PositionRelativeToGameObject.GetComponent<Collider>().bounds.size.y/2 + 0.1f);
+            newPosition = PositionRelativeToGameObject.transform.position + (PositionRelativeToGameObject.transform.up * offset);
+        }
+	    StartCoroutine(PositionObject(gameObject.transform, gameObject.transform.position, newPosition, 0.3f));
+	    StartCoroutine(RotateObject(gameObject.transform, gameObject.transform.rotation, PositionRelativeToGameObject.transform.rotation, 0.3f));
+    }
+
+    IEnumerator RotateObject(Transform thisTransform, Quaternion startRotation, Quaternion endRotation, float time)
+    {
+        thisTransform.rotation = Quaternion.Lerp(startRotation, endRotation, Time.deltaTime);
+        yield return 0;
+    }
+
+    IEnumerator PositionObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+            thisTransform.position = Vector3.Lerp(startPos, endPos, Time.deltaTime);
+            yield return 0;    
+    }
+
+
+}
---
+++ Assets/ShowAdjustmentWindow.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class ShowAdjustmentWindow : MonoBehaviour
+{
+
+    public enum CurrentStatus
+    {
+        Idle = 1,
+        WaitingForDelay = 2,
+        DelayDone = 3,
+        ShowingAdjustmentWindow = 4,
+    }
+
+    public float ShowWindowDelay;
+    public GameObject AdjustmentWindow;
+
+    private float _timeLeft;
+    private bool _gazeHasEnteredObject = false;
+    private CurrentStatus _currentStatus;
+    private int _exitedCount;
+
+    void Start()
+    {
+        _timeLeft = ShowWindowDelay;
+        _currentStatus = CurrentStatus.Idle;
+        AdjustmentWindow.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (_currentStatus == CurrentStatus.Idle) return;
+
+        _timeLeft -= Time.deltaTime;
+
+        if (_timeLeft < 0 && _currentStatus != CurrentStatus.DelayDone)
+        {
+            _currentStatus = CurrentStatus.DelayDone;
+            AdjustmentWindow.SetActive(true);
+        }
+
+    }
+
+    void OnGazeEnter()
+    {
+        _currentStatus = CurrentStatus.WaitingForDelay;
+    }
+
+    void OnGazeLeave()
+    {
+
+        _exitedCount++;
+        if (_exitedCount > 1)
+        {
+            Debug.Log("Gömmer rutan");
+            _timeLeft = ShowWindowDelay;
+            _currentStatus = CurrentStatus.Idle;
+            _exitedCount = 0;
+            AdjustmentWindow.SetActive(false);
+        }
+    }
+}
